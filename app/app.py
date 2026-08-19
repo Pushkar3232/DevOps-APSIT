a9efea6ceba9045fda479b9d1751a440011eb4a2
Pushkar141 <pushkarshinde3232@gmail.com>
2026-08-19 18:11:16 +0530

    feat: add Docker deployment version 2

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from DevOps APSIT!"
+    return "Hello from DevOps APSIT - Docker v2!"
 
 
 if __name__ == "__main__":
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,4 +11,4 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.data == b"Hello from DevOps APSIT!"
+    assert response.data == b"Hello from DevOps APSIT - Docker v2!"
